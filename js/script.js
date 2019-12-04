d9875704f567efa07ba34dd11028cf8e592fbfa4
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2019-12-04 11:57:13 +0100

    Update script logic

--- js/script.js
+++ js/script.js
@@ -1,12 +1,34 @@
-let playerMove = 'papier';
-printMessage('Player one zagrał ' + playerMove + ' Jeśli ruch komputera to nożyce, player one przegrał!');
+// let playerMove = 'papier';
+// printMessage('Player one zagrał ' + playerMove + ' Jeśli ruch komputera to nożyce, player one przegrał!');
 let randomFraction = Math.random();
-printMessage('Wylosowany ułamek to: '+randomFraction);
+printMessage('Wylosowany ułamek to: '+ randomFraction);
 let calculation = randomFraction * 3 + 1;
-printMessage('Wylosowany ułamek pomnożony przez 3 i dodany 1: '+calculation);
+printMessage('Wylosowany ułamek pomnożony przez 3 i dodany 1: ' + calculation);
 let roundNumber = Math.floor(calculation);
-printMessage('Zaokrąglona liczba to: '+roundNumber);
+printMessage('Zaokrąglona liczba to: ' + roundNumber);
+let randomNumber1 = Math.floor(Math.random()*11+9);
+printMessage('losowanie 11 -19 -> ' + randomNumber1);
+
 let randomNumber = Math.floor(Math.random()*3+1);
-console.log('Wylosowana liczba to: '+randomNumber);
-let randomNumber1 = Math.floor(Math.random()*12+8);
-printMessage('losowanie 11 -19 -> ' +randomNumber1);
+console.log('Wylosowana liczba to: '+ randomNumber);
+
+let computerMove = 'nieznany ruch';
+if(randomNumber == 1){
+    computerMove = '<strong>kamień</strong>';
+}
+    else if(randomNumber == 2){
+        computerMove = '<strong>papier</strong>';
+    }
+    else if(randomNumber == 3){
+        computerMove == '<strong>nożyce</strong>';
+    }
+
+printMessage('Mój ruch to :' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch: 1-kamień, 2-papier, 3-nożyce');
+console.log('Gracz wybrał...: ' + playerInput);
+let playerMove = 'nieznany ruch';
+if(playerInput == 1){
+    playerMove = '<strong>kamień</strong>';
+}
+printMessage('Twój ruch: ' + playerMove);
