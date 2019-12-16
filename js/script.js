6a057a3b6f6df05ffc4a1f2782174094f1cc9f48
Tomek Piotrowski <tpiotrowski007@gmail.com>
2019-12-16 23:40:27 +0100

    Delete commented code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-// let randomFraction = Math.random();
-// printMessage('Wylosowany ułamek to: '+ randomFraction);
-// let calculation = randomFraction * 3 + 1;
-// printMessage('Wylosowany ułamek pomnożony przez 3 i dodany 1: ' + calculation);
-// let roundNumber = Math.floor(calculation);
-// printMessage('Zaokrąglona liczba to: ' + roundNumber);
-// let randomNumber1 = Math.floor(Math.random()*11+9);
-// printMessage('losowanie 11 -19 -> ' + randomNumber1);
 {
     printMessage('<hr>');
     const playGame = function(playerInput){
@@ -27,7 +19,6 @@
     }
     let computerMove = getMoveName(randomNumber);
     printMessage('Mój ruch to: ' + computerMove);
-    // let playerInput = prompt('Wybierz swój ruch: 1-kamień, 2-papier, 3-nożyce');
     console.log('Gracz wybrał: ' + playerInput);
     let playerMove = getMoveName(playerInput);
     printMessage('Twój ruch: ' + playerMove);
