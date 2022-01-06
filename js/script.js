792288a15db9f9541eb19ff6252c674e009d72c0
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2022-01-06 09:46:31 +0100

    Add first function with variable - let

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let computerMove = "kamień";
+printMessage("Cześć, ja zagrałem " + computerMove + " Jeśli Twój ruch to papier, to wygrywasz!");
+let playerMove = "papier";
+printMessage("Hey, zagrałeś " + playerMove + "! Jeśli mój ruch to kamień, to wygrałeś :)");
