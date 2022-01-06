2dc0d7b5e7d4c275e595ddb2ffe29dc2737bdaa7
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2022-01-06 12:26:14 +0100

    Add Game result

--- js/script.js
+++ js/script.js
@@ -26,3 +26,24 @@ if (playerInput == 1){
     playerMove = "nożyce";
 }
 printMessage("Twój ruch graczu to: " + playerMove);
+if(computerMove == "kamień" && playerMove == "papier"){
+    printMessage('Ty wygrywasz!');
+} else if(computerMove == "papier" && playerMove == "kamień"){
+    printMessage('Komputer wygrywa.');
+} else if(computerMove == "nożyce" && playerMove == "papier"){
+    printMessage('Komputer wygrywa.');
+} else if(computerMove == "papier" && playerMove == "nożyce"){
+    printMessage('Ty wygrywasz!');
+} else if(computerMove == "nożyce" && playerMove == "kamień"){
+    printMessage('Ty wygrywasz!');
+} else if(computerMove == "kamień" && playerMove == "nożyce"){
+    printMessage('Komputer wygrywa.');
+} else if(computerMove == "kamień" && playerMove == "kamień"){
+    printMessage('Remis!');
+} else if(computerMove == "papier" && playerMove == "papier"){
+    printMessage('Remis!');
+} else if(computerMove == "nożyce" && playerMove == "nożyce"){
+    printMessage('Remis!');
+} else if(playerMove == 'nieznany ruch'){
+    printMessage('nieznany ruch gracza');
+} else printMessage('nieznany błąd');    
