0549e3cb8f14e87b02dfdeffc17dced69dc4ee6a
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2022-07-04 07:11:17 +0200

    Fix numerous bugs in script.js game should work

--- js/script.js
+++ js/script.js
@@ -1,35 +1,26 @@
-{
-  printMessage('<hr>');
-  const playGame = function(playerInput){
-    const score = displayResult;
-    clearMessage();
-    const randomNumber = Math.floor(Math.random()*3+1);
-    console.log('Wylosowana liczba to: '+ randomNumber);
-    const getMoveName = function(argMoveID){
-      if(argMoveID == 1){
-          return stone;
-        } else if(argMoveID == 2){
-          return paper;
-        } else if(argMoveID == 3){
-          return scissors;
-        } else {
-          printMessage('Nie znam ruchu o ID ' + argMoveID + '.');
-          return 'nieznany ruch';
-        }
+printMessage('<hr>');
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+const score = displayResult;
+// clearMessage();
+const randomNumber = Math.floor(Math.random()*3+1);
+console.log('Wylosowana liczba to: '+ randomNumber);
+const getMoveName = function(argMoveID){
+  if(argMoveID == 1){
+      return stone;
+    } else if(argMoveID == 2){
+      return paper;
+    } else if(argMoveID == 3){
+      return scissors;
+    } else {
+      printMessage('Nie znam ruchu o ID ' + argMoveID + '.');
+      return 'nieznany ruch';
     }
-    const computerMove = getMoveName(randomNumber);
-    printMessage('Mój ruch to: ' + computerMove);
-    console.log('Gracz wybrał: ' + playerInput);
-    const playerMove = getMoveName(playerInput);
-    printMessage('Twój ruch: ' + playerMove);
-    score(computerMove, playerMove);
-    console.log('Ruchy graczy: ', computerMove, playerMove);
-    printMessage('<hr>');
   }
-    const playButton1 = document.getElementById('play-stone');
-    playButton1.addEventListener('click', function(){playGame(1)});
-    const playButton2 = document.getElementById('play-paper');
-    playButton2.addEventListener('click', function(){playGame(2)});
-    const playButton3 = document.getElementById('play-scissors');
-    playButton3.addEventListener('click', function(){playGame(3)});
-}
+const computerMove = getMoveName(randomNumber);
+printMessage('Mój ruch to: ' + computerMove);
+console.log('Gracz wybrał: ' + playerInput);
+const playerMove = getMoveName(playerInput);
+printMessage('Twój ruch: ' + playerMove);
+score(computerMove, playerMove);
+console.log('Ruchy graczy: ', computerMove, playerMove);
+printMessage('<hr>');
