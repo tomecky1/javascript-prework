ce1973662df0f77ceb79cc8cd15e39e8f26c7bd7
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2022-07-05 15:03:19 +0200

    Fix conditions in functions.js and playerInput in script.js. Add pointer cursor in CSS

--- js/functions.js
+++ js/functions.js
@@ -1,35 +1,22 @@
 const stone = 'kamień';
 const paper = 'papier';
 const scissors = 'nożyce';
-function printMessage(msg){
+
+function printMessage(msg) {
     const div = document.createElement('div');
     div.innerHTML = msg;
     document.getElementById('messages').appendChild(div);
 }
-function clearMessage(){
+
+function clearMessage() {
     document.getElementById('messages').innerHTML = '';
 }
-function displayResult(argComputerMove, argPlayerMove){
+
+function displayResult(argComputerMove, argPlayerMove) {
     printMessage('Ja zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-    if(argComputerMove == stone && argPlayerMove == paper){
-        printMessage('Ty wygrywasz!');
-    } else if(argComputerMove == paper && argPlayerMove == stone){
-        printMessage('Komputer wygrywa.');
-    } else if(argComputerMove == scissors && argPlayerMove == paper){
-        printMessage('Komputer wygrywa.');
-    } else if(argComputerMove == paper && argPlayerMove == scissors){
-        printMessage('Ty wygrywasz!');
-    } else if(argComputerMove == scissors && argPlayerMove == stone){
-        printMessage('Ty wygrywasz!');
-    } else if(argComputerMove == stone && argPlayerMove == scissors){
-        printMessage('Komputer wygrywa.');
-    } else if(argComputerMove == stone && argPlayerMove == stone){
+    if (argComputerMove === argPlayerMove) {
         printMessage('Remis!');
-    } else if(argComputerMove == paper && argPlayerMove == paper){
-        printMessage('Remis!');
-    } else if(argComputerMove == scissors && argPlayerMove == scissors){
-        printMessage('Remis!');
-    } else if(argPlayerMove == 'nieznany ruch'){
-        printMessage('nieznany ruch gracza');
-        } else printMessage('nieznany błąd');    
+    } else if ((argComputerMove === paper && argPlayerMove === stone) || (argComputerMove === stone && argPlayerMove === scissors) || (argComputerMove === scissors && argPlayerMove === paper)) {
+        printMessage('Komputer wygrywa.');
+    } else printMessage('Graczu wygrywasz! :)');
 }
