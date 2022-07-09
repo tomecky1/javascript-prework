b717a9ad772d27cd56fd7fc2860c940c801e5f66
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2022-07-09 13:55:37 +0200

    Add changes from module 6.1 to script.js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,40 @@
-function playGame(playerInput) {
-    clearMessage();
-    printMessage('<hr>');
-    // const playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-    const score = displayResult;
-    const randomNumber = Math.floor(Math.random() * 3 + 1);
-    console.log('Wylosowana liczba to: ' + randomNumber);
-    const getMoveName = function (argMoveID) {
-        if (argMoveID === 1) {
-            return stone;
-        } else if (argMoveID === 2) {
-            return paper;
-        } else if (argMoveID === 3) {
-            return scissors;
-        } else {
-            printMessage('Nie znam ruchu o ID ' + argMoveID + '.');
-            return 'nieznany ruch';
+{
+    function playGame(playerInput) {
+        clearMessage();
+        printMessage('<hr>');
+        // const playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+        const score = displayResult;
+        const randomNumber = Math.floor(Math.random() * 3 + 1);
+        console.log('Wylosowana liczba to: ' + randomNumber);
+        const getMoveName = function (argMoveID) {
+            if (argMoveID === 1) {
+                return stone;
+            } else if (argMoveID === 2) {
+                return paper;
+            } else if (argMoveID === 3) {
+                return scissors;
+            } else {
+                printMessage('Nie znam ruchu o ID ' + argMoveID + '.');
+                return 'nieznany ruch';
+            }
         }
+        const computerMove = getMoveName(randomNumber);
+        printMessage('Mój ruch to: ' + computerMove);
+        console.log('Gracz wybrał: ' + playerInput);
+        const playerMove = getMoveName(playerInput);
+        printMessage('Twój ruch: ' + playerMove);
+        score(computerMove, playerMove);
+        console.log('Ruchy graczy: ', computerMove, playerMove);
+        printMessage('<hr>');
     }
-    const computerMove = getMoveName(randomNumber);
-    printMessage('Mój ruch to: ' + computerMove);
-    console.log('Gracz wybrał: ' + playerInput);
-    const playerMove = getMoveName(playerInput);
-    printMessage('Twój ruch: ' + playerMove);
-    score(computerMove, playerMove);
-    console.log('Ruchy graczy: ', computerMove, playerMove);
-    printMessage('<hr>');
-}
 
-document.getElementById('play-paper').addEventListener('click', function () {
-    playGame(2);
-});
-document.getElementById('play-stone').addEventListener('click', function () {
-    playGame(1);
-});
-document.getElementById('play-scissors').addEventListener('click', function () {
-    playGame(3);
-});
+    paperBtn = document.getElementById('play-paper').addEventListener('click', function () {
+        playGame(2);
+    });
+    stoneBtn = document.getElementById('play-stone').addEventListener('click', function () {
+        playGame(1);
+    });
+    scissorsBtn = document.getElementById('play-scissors').addEventListener('click', function () {
+        playGame(3);
+    });
+}
